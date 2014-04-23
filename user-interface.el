917656ca216351906e7d11967181b993bf9edc8d
Carlo Bertini <waydotnet@gmail.com>
2014-04-23 09:09:03 +0200

    remove header-line

--- user-interface.el
+++ user-interface.el
@@ -66,12 +66,12 @@
 ;; Show the current function name in the header line
 (which-function-mode)
 (setq which-func-unknown "n/a")
-(setq-default header-line-format
-              '((which-func-mode ("" which-func-format " "))))
-(setq mode-line-misc-info
-      ;; We remove Which Function Mode from the mode line, because it's mostly
-      ;; invisible here anyway.
-      (assq-delete-all 'which-func-mode mode-line-misc-info))
+;; (setq-default header-line-format
+;;               '((which-func-mode ("" which-func-format " "))))
+;; (setq mode-line-misc-info
+;;       ;; We remove Which Function Mode from the mode line, because it's mostly
+;;       ;; invisible here anyway.
+;;       (assq-delete-all 'which-func-mode mode-line-misc-info))
 
 
 
